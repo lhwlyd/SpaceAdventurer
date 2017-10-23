471c77e5ceedbc6cd25cf9bf79f1adfbf5d0b201
philliplagoc <philliplagoc@gmail.com>
2017-10-22 18:45:15 -0700

    editing Fast Enemy Script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -39,6 +39,7 @@ public class Enemy : MonoBehaviour {
         if(GameManager.instance.doingSetup){
             return;
         }
+        // Default enemy movement will be to always float towards player.
         float yDir = (target.position.y - this.transform.position.y);
         float xDir = (target.position.x - this.transform.position.x);
         float vSquare = Mathf.Pow(xDir, 2) + Mathf.Pow(yDir, 2);
--- Assets/Scripts/FastEnemy.cs
+++ Assets/Scripts/FastEnemy.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 /**
  * This class represents a fast enemy.
+ * This enemy will locate the player then charge at the player in a linear fashion. If this enemy misses the player, this enemy will 
+ * re-adjust itself to linearly charge at the player again.
  */
 public class FastEnemy : Enemy {
+    public GameObject explosion;
+    public bool hasCharged;
 
-	
+    /**
+     * Movement logic: Get player's position (Do not keep updating the player's position. Only 
+     * reset target once after done charging.)
+     * Move towards that position linearly (1 direction/ axis)
+     * Stop once at the player's position
+     */ 
+    protected override void FixedUpdate() {
+        if (GameManager.instance.doingSetup)
+            return;
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            GameObject thisExplosion = Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity) as GameObject;
+            Destroy(thisExplosion, 2f);
+        }
+    }
 }
