89de69d99172040576f6f98b306eadfd8393826a
philliplagoc <philliplagoc@gmail.com>
2017-10-23 21:51:36 -0700

    edited fast enemy script

--- Assets/Scripts/FastEnemy.cs
+++ Assets/Scripts/FastEnemy.cs
@@ -7,9 +7,33 @@ using UnityEngine;
  * This enemy will locate the player then charge at the player in a linear fashion. If this enemy misses the player, this enemy will 
  * re-adjust itself to linearly charge at the player again.
  */
-public class FastEnemy : Enemy {
+public class FastEnemy : Enemy
+{
     public GameObject explosion;
-    public bool hasCharged;
+    public bool hasCharged = true; // Whether or not the enemy has charged
+    float xDir;
+    float yDir;
+
+    private Animator animator;
+    private Transform target;
+
+    protected override void Start()
+    {
+        //Register this enemy with our instance of GameManager by adding it to a list of Enemy objects. 
+        //This allows the GameManager to issue movement commands.
+        GameManager.instance.AddEnemyToList(this);
+
+        //Get and store a reference to the attached Animator component.
+        animator = GetComponent<Animator>();
+
+        //Find the Player GameObject using it's tag and store a reference to its transform component.
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        circleCollider2d = GetComponent<CircleCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
     /**
      * Movement logic: Get player's position (Do not keep updating the player's position. Only 
@@ -17,15 +41,44 @@ public class FastEnemy : Enemy {
      * Move towards that position linearly (1 direction/ axis)
      * Stop once at the player's position
      */ 
-    protected override void FixedUpdate() {
+    protected override void FixedUpdate()
+    {
         if (GameManager.instance.doingSetup)
             return;
+
+        if(hasCharged)
+        {
+            // Reset target
+            yDir = (target.position.y - this.transform.position.y);
+            xDir = (target.position.x - this.transform.position.x);
+            hasCharged = false;
+        }
+
+        float vSquare = Mathf.Pow(xDir, 2) + Mathf.Pow(yDir, 2);
+
+        Vector2 velocity = new Vector2(xDir, yDir);
+        // Control the velocity's value to be always speed^2
+        if (vSquare > speed * speed)
+        {
+            velocity *= (Mathf.Pow(speed, 2) / vSquare);
+
+        }
+        else
+        {
+            velocity.Normalize();
+            velocity *= speed;
+        }
+
+        rb2d.MovePosition(rb2d.position + velocity * Time.deltaTime);
     }
 
-    protected override void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Player") {
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
             GameObject thisExplosion = Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity) as GameObject;
             Destroy(thisExplosion, 2f);
         }
+        hasCharged = true;
     }
 }
