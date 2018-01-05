29b64f61e4aeb3b0d1d654b276ca4d8d916be474
lhwlyd <874036568@qq.com>
2018-01-04 22:20:04 -0800

    Updated Radar

---
+++ Assets/Scripts/EffectsScripts/UISpinner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISpinner : MonoBehaviour {
+
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(0,0,-1);
+    }
+}
